c3ef98ee4775474c52735bb15c6ccba9e148851e
lzj <13587859169@163.com>
2022-05-08 23:06:44 +0800

    first game

---
+++ My1/Assets/mScript/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public Text hello;
+	public void ananniu()
+	{
+		hello.text="hello world";
+	}
+}
